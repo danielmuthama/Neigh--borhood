1b3b701a494b1423658722181279ee8ae81978b5
danielmuthama <daniel.muthama@student.moringaschool.com>
2022-04-24 04:13:48 +0300

    update files

--- photos/models.py
+++ photos/models.py
@@ -11,9 +11,11 @@ class Category(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True, blank=True)
     name = models.CharField(max_length=100, null=False, blank=False)
+    id = models.AutoField(primary_key=True)
 
     def __str__(self):
         return self.name
+
 
 
 class Photo(models.Model):
@@ -25,6 +27,7 @@ class Photo(models.Model):
         Category, on_delete=models.SET_NULL, null=True, blank=True)
     image = models.ImageField(null=False, blank=False)
     description = models.TextField()
+    id = models.AutoField(primary_key=True)
 
     def __str__(self):
         return self.description
